749fc398e0f70979c825ee5dabfc61870abb8809
shmuir <shmuir1@gmail.com>
2024-05-09 16:15:03 -0700

    dynamic modeling

---
+++ dforestgrowth.R
@@ -0,0 +1,25 @@
+#' Forest growth model
+#' @author Sam Muir and Melissa Widas
+#' @param Time time period
+#' @param params  list of parameters with 3 values: r, g, and K:
+#'  r: pre canopy closure growth rate (exponential, for forests where C < threshold canopy closure )
+#'  g: post-canopy closure growth rate (linear)
+#'  K: carrying capacity in units of carbon
+#' @param C (initial) size of the forest in units of carbon
+#' @param thresh canopy closure threshold in units of carbon
+#'
+#' @return list: derivative of forest size with time
+#'
+#' @examples
+dforestgrowth <- function(Time, C, params, thresh) {
+  
+  if (C < thresh) {
+    dC_dT <- params$r * C
+    return(list(dC_dT))
+  }
+  else if (C >= thresh) {
+    dC_dT <- params$g * (1 - C / params$K)
+    return(list(dC_dT))
+  }
+  
+}
