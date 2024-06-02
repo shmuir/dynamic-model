50d601120c2d20beaf5648cbb4f4e295267e81b1
shmuir <shmuir1@gmail.com>
2024-06-02 10:50:38 -0700

    finishing lotka voltera

--- lotvmodK.R
+++ lotvmodK.R
@@ -1,13 +1,13 @@
 #' Lot. Voltera Model
 #'
-#' function computes the rate of change of populations in a predictor prey interaction
+#' function computes the rate of change of populations in a predictor prey interaction with hunting
 #' @param t  time (days)
 #' @param pop datatype list initial conditions; list with two values prey=number of prey and pred=number of predictor
 #' @param pars datatype list  coefficient in Lotka-Voltera pars$rprey, pars$alpha, pars$eff, par$pmort
 #'  \emph{rprey} is growth rate of prey population;
 #'  \emph{eff} is the rate of ingestion of prey by predators
 #'  \emph{alpha} is a interaction coefficient (higher values greater interaction)
-#'  \emph{beta} is the effect of predator hunting prey
+#'  \emph{beta} is the effect of hunting by humans
 #’  \emph{pmort}  mortality rate of predictor population
 #' @param thresh minimum threshold for hunting
 #' @examples
